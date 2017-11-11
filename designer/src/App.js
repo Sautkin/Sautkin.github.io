36b276c54644928813fdd1f62ed4a3911f173967
Sautkin <justcomer@gmail.com>
2017-11-12 03:28:04 +0700

    Adding decorators

--- designer/src/App.js
+++ designer/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
+@observer
 class App extends Component {
+  @observable q = 4;
   render() {
     return (
       <div className="App">
@@ -10,8 +14,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro" onClick={() => this.q++}>
+          { this.q }
         </p>
       </div>
     );
